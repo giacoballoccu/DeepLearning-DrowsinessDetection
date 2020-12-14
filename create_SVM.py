12158185f7c8767307aa85fe9d4509c3c6511254
DanyOrtu97 <danieleortu8@gmail.com>
2020-12-14 19:15:16 +0100

    Added the algorithm to detect blinks and started feature extraction for SVM

---
+++ create_SVM.py
@@ -0,0 +1,27 @@
+import os
+import csv
+from detect_blinks import *
+
+path = "/home/dani/Scrivania/DLA/"
+detect_blinks = "detect_blinks.py"
+
+print("....Video watching!!")
+
+for video in os.listdir(path):
+    N_Blinks = 0
+
+    #Only 0 and 10 videos
+    if(video.split(".")[0] != '5'):
+        if (video.split(".")[0] == '10'):
+            label = 1
+        else:
+            label = 0
+
+        result_file = detect("/home/dani/PycharmProjects/ProgettoDLA-Puglisi-/blink_detection_demo.mp4")
+
+        with open('resultCSV.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([result_file[0], label, result_file[1]]) #NumeroBLinks, label, ear
+
+        #control ear!!!!!!!!!!!!!!!!!!!!!
+
